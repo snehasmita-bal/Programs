afba50aee662645d466e7a6aee7d501d04979edb
Snehasmita Bal <smitasneha.bal13@gmail.com>
2024-08-02 11:02:07 +0530

    Sum of 2 Numbers

---
+++ Programs/src/Programs/Sum.java
@@ -0,0 +1,18 @@
+package Programs;
+import java.util.*;
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    
+	    int c=a+b;
+	    System.out.println("Sum of two numbers:" +c);
+	}
+	
+
+}
